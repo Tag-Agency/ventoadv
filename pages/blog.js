04b8935c0edde7a2de9a5c00a3ee94f2b1e942f0
Mauro Biancu <mauro.biancu@gmail.com>
2025-10-31 00:23:09 +0100

    Fix blog page arrow span and hydration errors

--- pages/blog.js
+++ pages/blog.js
@@ -53,7 +53,6 @@ export default function BlogPage() {
                       <p className="text-gray-600 mb-4">{post.excerpt}</p>
                       <div className="flex items-center justify-between">
                         <span className="text-gray-700">Di {post.author}</span>
-                        <span className="text-[#d2ad40]">
                           <span className="text-[#d2ad40]">→</span>
                       </div>
                     </div>
